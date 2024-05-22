e98b7dc8722faa065de286f7448b7316fc4ed923
Moovaa2 <128578152+Moovaa2@users.noreply.github.com>
2024-05-22 10:54:15 +0200

    Création d'une fonction pour intéragir avec le script du bot suite à l'appel de la fonction sendURL("LIEN")

--- script_bot.py
+++ script_bot.py
@@ -10,43 +10,47 @@ import nest_asyncio
 
 nest_asyncio.apply()
 
-async def my_coroutine():
-    print("Hello, World!")
-
-async def main():
-    await my_coroutine()
-
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-
-#Token du bot discord
-BOT_TOKEN = "TOKEN"
-
-#Définir le channel concerné pour l'envoi de l'URL
-CHANNEL_ID = 1242449552850681958
-
-#Fonction pour envoyer un message au canal spécifié
-async def send_message_to_channel(channel_id, message):
-    channel = client.get_channel(channel_id)
-    if channel:
-        await channel.send(message)
-    else:
-        print("L'identifiant du channel saisi est incorrect")
-
-#Obtention de l'URL de la dernière vidéo uploadée
-URL = "test"
-@client.event
-async def on_ready():    
-    await send_message_to_channel(CHANNEL_ID, "Voici l'URL de la dernière vidéo qui a été uploadée : " + URL)
+def sendURL(URL):
+    async def my_coroutine():
+        print("Hello, World!")
     
+    async def main():
+        await my_coroutine()
     
-async def run_bot():
-    bot_task = asyncio.create_task(client.start(BOT_TOKEN))
-    await asyncio.sleep(10)
-    await client.close()
+    intents = discord.Intents.default()
+    client = discord.Client(intents=intents)
     
-def main():
-    asyncio.run(run_bot())
+    #Token du bot discord
+    BOT_TOKEN = "TOKEN"
+    
+    #Définir le channel concerné pour l'envoi de l'URL
+    CHANNEL_ID = 1242449552850681958
+    
+    #Fonction pour envoyer un message au canal spécifié
+    async def send_message_to_channel(channel_id, message):
+        channel = client.get_channel(channel_id)
+        if channel:
+            await channel.send(message)
+        else:
+            print("L'identifiant du canal est incorrect, faites un clique droit sur le canal concerné pour récupérer l'identifiant")
+    
+    #Envoi de l'URL lors du lancement du serveur
+    @client.event
+    async def on_ready():    
+        await send_message_to_channel(CHANNEL_ID, "Voici l'URL de la dernière vidéo qui a été publiée : \n" + URL)
+        
+    async def run_bot():
+        bot_task = asyncio.create_task(client.start(BOT_TOKEN))
+        await asyncio.sleep(5)
+        await client.close()
+        
+    def main():
+        asyncio.run(run_bot())
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
+        
+sendURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+sendURL("https://www.youtube.com/")
+sendURL("https://google.com")
+        
