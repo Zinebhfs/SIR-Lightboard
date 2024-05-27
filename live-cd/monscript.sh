a9a0aeb2f85f5ea9160f9ac724e13a5e2a6e73c2
Alan <alan.jumeaucourt@gmail.com>
2024-05-28 00:19:29 +0200

    Launch obs, made it full screen then launch the python app

--- live-cd/monscript.sh
+++ live-cd/monscript.sh
@@ -1,6 +1,23 @@
 #! /bin/sh
 
-touch /home/user/Test
-echo "Test" > /home/user/Tes
-echo "Test encore une fois" >> /home/user/Test
+set -ex
 
+# Launch obs and made it fullscreen
+apt install xdotool -y
+obs --profile /home/user/.config/obs-studio/profiles/untitled/basic &
+sleep 10
+WID=`xdotool search --name "OBS"`
+echo $WID
+xdotool windowfocus $WID.
+xdotool mousemove --window $WID 0 0
+xdotool mousemove --window $WID 100 100
+xdotool click --window $WID 3
+xdotool key Down Down Down Down Right Return
+
+
+apt install wget -y
+wget https://github.com/Zinebhfs/SIR-Lightboard/archive/refs/tags/rc.tar.gz
+tar -xvf rc.tar.gz
+cd SIR-Lightboard-rc
+pip install -r requirements.txt
+python main.py
