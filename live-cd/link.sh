0123903fd7814b2b5e2188b70813e462075462a2
Zouzzou21 <lucas.landy@orange.fr>
2024-05-27 18:55:47 +0200

    live cd with script boot execution

--- live-cd/link.sh
+++
@@ -1,1 +0,0 @@
-ln -s /etc/init.d/monscript /etc/rc5.d/S9999monscript
---
+++ live-cd/monscript.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+touch /home/user/Mabite
+echo "Ma bite de test" > /home/user/Mabite
+echo "Ma bite de test encore une fois" >> /home/user/Mabite
+
