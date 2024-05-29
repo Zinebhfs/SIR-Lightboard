bca247bd2e42e7fcbb47a7b1d54896cd9f82ebae
Lucas LANDY <lucas.landy@insa-lyon.fr>
2024-05-29 15:04:06 +0200

    Update startup script and OBS configuration files

--- monscript.sh
+++ monscript.sh
@@ -6,22 +6,24 @@ set -ex
 
 # Copy obs global config
 mkdir -p /home/user/.config/obs-studio/
-cp /home/user/SIR-Lightboard-main/obs/obs-global-config.ini /home/user/.config/obs-studio/global.ini
+cp /home/user/SIR-Lightboard/obs/obs-global-config.ini /home/user/.config/obs-studio/global.ini
 chmod +777 /home/user/.config/obs-studio/global.ini
 
 # Copy obs profile
 mkdir -p /home/user/.config/obs-studio/basic/profiles/MyProfile
-cp /home/user/SIR-Lightboard-main/obs/obs-profile-config.ini /home/user/.config/obs-studio/basic/profiles/MyProfile/basic.ini
+cp /home/user/SIR-Lightboard/obs/obs-profile-config.ini /home/user/.config/obs-studio/basic/profiles/MyProfile/basic.ini
 chmod +777 /home/user//.config/obs-studio/basic/profiles/MyProfile/basic.ini
 
 # Copy obs scene
 mkdir -p /home/user/.config/obs-studio/basic/scenes
-cp /home/user/SIR-Lightboard-main/obs/obs-scene-config.json /home/user/.config/obs-studio/basic/scenes/MyScene.json
+cp /home/user/SIR-Lightboard/obs/obs-scene-config.json /home/user/.config/obs-studio/basic/scenes/MyScene.json
 chmod +777 /home/user/.config/obs-studio/basic/scenes/MyScene.json
+
+sudo pip install -r requirements.txt --break-system-packages
 
 obs &
 
 sleep 5
+# if web socket pas ouvert alors pas python A FAIRE
 
-sudo pip install -r requirements.txt --break-system-packages
-sudo python3 main.py
+sudo /usr/bin/python3 /home/user/SIR-Lightboard/main.py
