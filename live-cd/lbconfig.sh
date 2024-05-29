caea270f9dcf504178e6e86980cc6949e331b008
Lucas LANDY <lucas.landy@insa-lyon.fr>
2024-05-29 10:11:45 +0200

    Update monscript.sh right

--- live-cd/lbconfig.sh
+++ live-cd/lbconfig.sh
@@ -14,18 +14,20 @@ echo task-cinnamon-desktop > config/package-lists/desktop.list.chroot
 echo obs-studio >> config/package-lists/desktop.list.chroot
 echo pip >> config/package-lists/desktop.list.chroot
 echo wget >> config/package-lists/desktop.list.chroot
-echo xdotool >> config/package-lists/desktop.list.chroot
 echo kbd >> config/package-lists/desktop.list.chroot
 echo python3-tk >> config/package-lists/desktop.list.chroot
 
 # Copy xsessionrc and monscript.sh as startup script
 mkdir -p config/includes.chroot_after_packages/etc/skel/
 cp ../obs.xsession config/includes.chroot_after_packages/etc/skel/.xsessionrc
-cp ../monscript.sh config/includes.chroot_after_packages/etc/skel/
+chmod +777 config/includes.chroot_after_packages/etc/skel/.xsessionrc
+cp ../monscript.sh config/includes.chroot_after_packages/etc/skel/monscript.sh
+chmod +777 config/includes.chroot_after_packages/etc/skel/monscript.sh
 
 # Copy obs global config
 mkdir -p config/includes.chroot_after_packages/etc/skel/.config/obs-studio/
 cp ../obs-global-config.ini config/includes.chroot_after_packages/etc/skel/.config/obs-studio/global.ini
+chmod +777 config/includes.chroot_after_packages/etc/skel/.config/obs-studio/global.ini
 
 # Copy obs profile
 mkdir -p config/includes.chroot_after_packages/etc/skel/.config/obs-studio/basic/profiles/MyProfile
@@ -40,6 +42,9 @@ chmod +777 config/includes.chroot_after_packages/etc/skel/.config/obs-studio/bas
 # These config file for the main python app is not in git, because it's sensistive (API keys)
 echo "These config file for the main python app is not in git, because it's sensistive (API keys). If they are not present, this script will fail."
 mkdir -p config/includes.chroot_after_packages/etc/skel/SIR-Lightboard-main
-cp ../../.env config/includes.chroot_after_packages/etc/skel/SIR-Lightboard-main
-cp ../../token.pkl config/includes.chroot_after_packages/etc/skel/SIR-Lightboard-main
-cp ../../client_secret.json config/includes.chroot_after_packages/etc/skel/SIR-Lightboard-main
+cp ../../.env config/includes.chroot_after_packages/etc/skel/SIR-Lightboard-main/.env
+chmod +777 config/includes.chroot_after_packages/etc/skel/SIR-Lightboard-main/.env
+cp ../../token.pkl config/includes.chroot_after_packages/etc/skel/.config/obs-studio/basic/scenes/MyScene.json
+chmod +777 config/includes.chroot_after_packages/etc/skel/.config/obs-studio/basic/scenes/MyScene.json
+cp ../../client_secret.json config/includes.chroot_after_packages/etc/skel/SIR-Lightboard-main/client_secret.json
+chmod +777 config/includes.chroot_after_packages/etc/skel/SIR-Lightboard-main/client_secret.json
