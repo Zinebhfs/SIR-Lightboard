c97221d2d840fc4dc2442ec561f127674c84c955
alan jumeaucourt <alan.jumeaucourt@gmail.com>
2024-05-30 17:25:20 +0200

    chore: Update lbconfig.sh with proper formatting and line breaks

--- live-cd/lbconfig.sh
+++ live-cd/lbconfig.sh
@@ -3,8 +3,11 @@
 set -ex
 
 # Config 
-lb config --bootappend-live "boot=live locales=fr_FR.UTF-8 keyboard-layouts=fr username=user" --mode "debian" --system "live" --distribution "bookworm" --archive-areas "main contrib non-free non-free-firmware" --binary-images "iso-hybrid" --apt-recommends "false"
-
+lb config --bootappend-live "boot=live locales=fr_FR.UTF-8 keyboard-layouts=fr username=user" \
+          --mode "debian" --system "live" --distribution "bookworm" \
+          --archive-areas "main contrib non-free non-free-firmware" --binary-images "iso-hybrid"  \
+          --apt-recommends "false"
+          
 # Bootloader
 mkdir -p config/bootloaders/isolinux
 cp ../isolinux.cfg config/bootloaders/isolinux/
