1e5112a7e19952cb46ca7dca564c31e01582c8de
alan jumeaucourt <alan.jumeaucourt@gmail.com>
2024-05-30 17:41:47 +0200

    refactor: Update key bindings for starting and stopping recording

--- main.py
+++ main.py
@@ -229,13 +229,13 @@ class RecordingApp:
             exit()
 
     def on_press(self, event: KeyboardEvent) -> None:
-        if event.name == '"':
+        if event.name == '3':
             self.logger.info("Record key pressed: starting recording")
             self.start_recording()
-        elif event.name == 'é':
+        elif event.name == '2':
             self.logger.info("Stop key pressed: stopping recording")
             self.stop_recording()
-        # elif event.name == '&':
+        # elif event.name == '1':
         #     print("Quit key pressed")
         #     keyboard.unhook_all()
         #     self.obs_recorder.disconnect()
